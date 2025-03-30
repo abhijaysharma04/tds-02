41bb27c30e6f789b5dd813ea4299a7b621ba2479
Raj Sharma <raj.sharma@blueyonder.com>
2025-03-30 23:31:49 +0530

    Fix task A27

--- api/index.py
+++ api/index.py
@@ -136,10 +136,11 @@ def identify_task_code(question_text: str) -> str:
         return "A26"
     
     if (
-        "fastapi" in question_text 
-        and "students" in question_text 
-        and "class" in question_text 
-        and "query parameter" in question_text
+    "fastapi" in question_text
+    and "csv" in question_text
+    and "class" in question_text
+    and "students" in question_text
+    and ("query parameter" in question_text or "?class=" in question_text)
     ):
         return "A27"
 
